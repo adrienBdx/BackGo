bf4b12c15e39039bdc9b341f088914cc3c1291c9
ad <pietka.adrien@gmail.com>
2019-02-10 20:26:42 -0800

    GinJokes - Back and Front without Auth

---
+++ GinJokes/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	api.GET("/jokes", JokeHandler)            // List of all jokes
+	api.POST("/jokes/like/:jokeID", LikeJoke) // List of likes for a joke id
+
+	// Start and run the server
+	router.Run(":3000")
+}
