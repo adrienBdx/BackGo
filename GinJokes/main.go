5ec0b46d94e3efc102a0e27200629c7a05c744e5
ad <pietka.adrien@gmail.com>
2019-06-29 16:32:22 -0700

    Go - Learning updates

--- GinJokes/main.go
+++ GinJokes/main.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	"errors"
+	"log"
 	"net/http"
+	"os"
 
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
+var jwtMiddleWare *jwtmiddleware.JWTMiddleware
+
 func main() {
+
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			aud := os.Getenv("AUTH0_API_AUDIENCE")
+
+			checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+			if !checkAudience {
+				return token, errors.New("Invalid audience.")
+			}
+			// verify iss claim
+			iss := os.Getenv("AUTH0_DOMAIN")
+			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+			if !checkIss {
+				return token, errors.New("Invalid issuer.")
+			}
+
+			cert, err := getPemCert(token)
+			if err != nil {
+				log.Fatalf("could not get cert: %+v", err)
+			}
+
+			result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+			return result, nil
+		},
+		SigningMethod: jwt.SigningMethodRS256,
+	})
+
+	// register our actual jwtMiddleware
+	jwtMiddleWare = jwtMiddleware
+
 	// Set the router as the default one shipped with Gin
 	router := gin.Default()
 
@@ -24,8 +61,8 @@ func main() {
 		})
 	}
 
-	api.GET("/jokes", JokeHandler)            // List of all jokes
-	api.POST("/jokes/like/:jokeID", LikeJoke) // List of likes for a joke id
+	api.GET("/jokes" /*authMiddleware(),*/, JokeHandler)            // List of all jokes
+	api.POST("/jokes/like/:jokeID" /*authMiddleware(),*/, LikeJoke) // List of likes for a joke id
 
 	// Start and run the server
 	router.Run(":3000")
--- HelloWorldProject/hello.go
+++ HelloWorldProject/hello.go
@@ -2,50 +2,6 @@ package main
 
 import "fmt"
 
-import "encoding/json"
-import "log"
-
-// Avenger represents a single hero
-type Avenger struct {
-	RealName string `json:"real_name"`
-	HeroName string `json:"hero_name"`
-	Planet   string `json:"planet"`
-	Alive    bool   `json:"alive"`
-}
-
-func (a *Avenger) isAlive() {
-	a.Alive = true
-}
-
 func main() {
 	fmt.Printf("hello, world\n")
-	fmt.Print("Press enter")
-	//bufio.NewReader(os.Stdin).ReadBytes('\n')
-
-	avengers := []Avenger{
-		{
-			RealName: "Dr. Bruce Banner",
-			HeroName: "Hulk",
-			Planet:   "Midgard",
-		},
-		{
-			RealName: "Tony Stark",
-			HeroName: "Iron Man",
-			Planet:   "Midgard",
-		},
-		{
-			RealName: "Thor Odinson",
-			HeroName: "Thor",
-			Planet:   "Midgard",
-		},
-	}
-
-	avengers[1].isAlive()
-
-	jsonBytes, err := json.Marshal(avengers)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(string(jsonBytes))
-
 }
