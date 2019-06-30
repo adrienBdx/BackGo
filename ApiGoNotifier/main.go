e5cf6a403c35795efc2a742c489b1247b94c070b
ad <pietka.adrien@gmail.com>
2019-06-30 09:20:07 -0700

    Notify for each new article on URL
    Test OK on Windows 10

--- ApiGoNotifier/main.go
+++ ApiGoNotifier/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
+	"unicode"
 
 	"github.com/gen2brain/beeep"
 	"github.com/gocolly/colly"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
 )
 
 var articleIds []int
 var articleTitles []string
+var notFirstTurn = false
 
 func alert(title string, bodyMessage string) {
 
@@ -18,8 +23,6 @@ func alert(title string, bodyMessage string) {
 	if alert != nil {
 		panic(alert)
 	}
-
-	fmt.Println("hello world - " + title + ":" + bodyMessage)
 }
 
 func main() {
@@ -31,16 +34,15 @@ func main() {
 		for t := range ticker.C {
 			fmt.Println("Tick at", t)
 
-			alert("Ticking", "First tick")
+			readWebsite("https://www.lemonde.fr/", "span[class=article__title-label]", notFirstTurn)
 		}
 	}()
 
-	//time.Sleep(720 * time.Hour) ToDo Remove
+	time.Sleep(720 * time.Hour)
 	ticker.Stop()
 
-	readWebsite()
+	alert("Shutdown", "This program is running since 720h, 1 month, and will stop. Take a rest.")
 
-	fmt.Println("test")
 	/*
 		c.Visit("https://www.boursorama.com/bourse/devises/taux-de-change-euro-dollarcanadien-EUR-CAD/")
 		// span[c-instrument c-instrument--last]
@@ -52,9 +54,89 @@ func main() {
 	*/
 
 	fmt.Println("hello world")
-
 }
 
+func readWebsite(targetURL string, targetHTML string, activeAlert bool) {
+	c := colly.NewCollector()
+
+	c.OnHTML(targetHTML, func(e *colly.HTMLElement) {
+
+		var title = e.Text
+		var id = createID(title)
+
+		if !ContainsInt(articleIds, id) {
+			articleTitles = append(articleTitles, title)
+			articleIds = append(articleIds, id)
+
+			if activeAlert {
+				alert("New article", tryCleanFormat(title))
+			}
+		}
+
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.Visit(targetURL)
+
+	notFirstTurn = true
+}
+
+func createID(titleToConvert string) int {
+
+	if len(titleToConvert) == 0 {
+		return 0
+	}
+
+	var createdID int
+
+	for pos, char := range titleToConvert {
+		createdID += int(char) + pos
+	}
+
+	return createdID
+}
+
+func ContainsInt(ids []int, value int) bool {
+
+	set := make(map[int]bool)
+	for _, v := range ids {
+		set[v] = true
+	}
+
+	return set[value]
+}
+
+func isMn(r rune) bool {
+	return unicode.Is(unicode.Mn, r) // Mn: nonspacing marks
+}
+
+/// ToDo find the right way without breaking the sentence
+func tryCleanFormat(title string) string {
+
+	title = strings.ReplaceAll(title, "é", "e")
+	title = strings.ReplaceAll(title, "à", "a")
+	title = strings.ReplaceAll(title, "'", " ")
+	title = strings.ReplaceAll(title, "’", " ")
+	title = strings.ReplaceAll(title, "«", " ")
+	title = strings.ReplaceAll(title, "»", " ")
+
+	t := transform.Chain(norm.NFD, transform.RemoveFunc(isMn), norm.NFC)
+	titleNormalized, _, _ := transform.String(t, title)
+
+	/*reg, err := regexp.Compile("[^a-zA-Z0-9 ]+ ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	titleNormalized = reg.ReplaceAllString(title, "")*/
+	fmt.Println(title + " ||| " + titleNormalized)
+
+	return titleNormalized
+}
+
+/*
 func readWebsite() {
 	c := colly.NewCollector()
 	c.OnHTML("span[class=article__title-label]", func(e *colly.HTMLElement) {
@@ -74,3 +156,4 @@ func readWebsite() {
 
 	c.Visit("https://www.lemonde.fr/")
 }
+*/
