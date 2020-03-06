90522186adc127769f382ea50b498642caaec736
ApiBdx <pietka.adrien@gmail.com>
2020-03-06 00:51:12 -0800

    Blockchain struture GET/POST

--- GoBlock/go.mod
+++ GoBlock/go.mod
@@ -3,7 +3,7 @@ module github.com/BackGo/GoBlock
 go 1.13
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gorilla/mux v1.7.4
 	github.com/joho/godotenv v1.3.0 // indirect
 )
--- GoBlock/main.go
+++ GoBlock/main.go
@@ -1,7 +1,148 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+)
+
+// Types
+
+var Blockchain []Block
+
+type Block struct {
+	Index     int
+	Timestamp string
+	BPM       int
+	Hash      string
+	PrevHash  string
+}
+
+type Message struct {
+	BPM int
+}
+
+// Func
 func main() {
-	fmt.Printf("hello, world\n")
+
+}
+
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	hash := sha256.New()
+	hash.Write([]byte(record))
+	hashed := hash.Sum(nil)
+
+	return hex.EncodeToString(hashed)
+}
+
+func generateBlock(previousBlock Block, BPM int) (Block, error) {
+
+	var newBlock Block
+
+	newBlock.Index = previousBlock.Index + 1
+	newBlock.Timestamp = time.Now().String()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = previousBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
+}
+
+func isBlockValid(newBlock, oldBlock Block) bool {
+
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+}
+
+func replaceChain(newBlocks []Block) {
+	if len(newBlocks) > len(Blockchain) {
+		Blockchain = newBlocks
+	}
+}
+
+// API
+func run() error {
+	mux := makeMuxRouter()
+	httpAddr := os.Getenv("ADDR")
+	log.Println("Listening on ", os.Getenv("ADDR"))
+
+	server := &http.Server{
+		Addr:           ":" + httpAddr,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func makeMuxRouter() http.Handler {
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+	muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
+	return muxRouter
+}
+
+func handleGetBlockchain(writer http.ResponseWriter, r *http.Request) {
+	bytes, err := json.MarshalIndent(Blockchain, "", "  ")
+
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
+
+	io.WriteString(writer, string(bytes))
+}
+
+func handleWriteBlock(writer http.ResponseWriter, request *http.Request) {
+	var message Message
+
+	decoder := json.NewDecoder(request.Body)
+
+	if err := decoder.Decode(&m); err != nil {
+		respondWithJson(writer, request, http.StatusBadRequest, request.Body)
+		return
+	}
+	//ToDo check
+	defer request.Body.Close()
+
+	newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], message.BPM)
+	if err != nil {
+		respondWithJson(writer, request, http.StatusInternalServerError, message)
+		return
+	}
+	if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+		newBlockchain := append(Blockchain, newBlock)
+		replaceChain(newBlockchain)
+		spew.Dump(Blockchain)
+	}
+
+	respondWithJSON(writer, request, http.StatusCreated, newBlock)
+
 }
